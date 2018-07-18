ceccd701a08611b1dba7686dd68434027b97e770
v00rix <vladogim97@gmail.com>
2018-07-18 23:27:02 +0200

    Initial commit - simple window opening setup

---
+++ main.cpp
@@ -0,0 +1,65 @@
+// OpenGL
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main() {
+
+    //region Initialization
+    GLFWwindow *window;
+
+    if (!glfwInit()) {
+        fprintf(stderr, "Failed to initialize GLFW\n");
+        getchar();
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Open a window and create its OpenGL context
+    window = glfwCreateWindow(1024, 768, "OpenGL", nullptr, nullptr);
+    if (window == nullptr) {
+        fprintf(stderr, "Failed to open GLFW window.\n");
+        getchar();
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    // Initialize GLEW
+    glewExperimental = GL_TRUE; // Needed for core profile
+    if (glewInit() != GLEW_OK) {
+        fprintf(stderr, "Failed to initialize GLEW\n");
+        getchar();
+        glfwTerminate();
+        return -1;
+    }
+    //endregion
+
+    // Ensure we can capture the escape key being pressed below
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+    do {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        //region Main loop
+
+        //endregion
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+    } while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+             glfwWindowShouldClose(window) == 0);
+
+    //region Termination
+    glfwTerminate();
+    //endregion
+
+    return 0;
+}
