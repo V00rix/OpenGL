e3a61018098bb71c8a7faae2d01cd3465e61f25d
v00rix <vladogim97@gmail.com>
2018-07-19 09:35:05 +0200

    Refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -3,30 +3,35 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-int main() {
+static GLFWwindow *window;
 
-    //region Initialization
-    GLFWwindow *window;
 
+//region Helpers
+
+static const bool initGLFW() {
     if (!glfwInit()) {
         fprintf(stderr, "Failed to initialize GLFW\n");
         getchar();
-        return -1;
+        return false;
     }
 
-    glfwWindowHint(GLFW_SAMPLES, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+    return true;
+}
+
+static bool createWindow() {
     // Open a window and create its OpenGL context
     window = glfwCreateWindow(1024, 768, "OpenGL", nullptr, nullptr);
     if (window == nullptr) {
         fprintf(stderr, "Failed to open GLFW window.\n");
         getchar();
         glfwTerminate();
-        return -1;
+        return false;
     }
 
     glfwMakeContextCurrent(window);
@@ -37,13 +42,23 @@ int main() {
         fprintf(stderr, "Failed to initialize GLEW\n");
         getchar();
         glfwTerminate();
-        return -1;
+        return false;
     }
+
+    return true;
+}
+//endregion
+
+int main() {
+    //region Initialization
+    if (!initGLFW()) { return -1; }
+    if (!createWindow()) { return -1; }
     //endregion
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
 
     // Ensure we can capture the escape key being pressed below
     glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
-
     do {
         glClear(GL_COLOR_BUFFER_BIT);
 
