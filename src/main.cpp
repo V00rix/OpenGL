6e6a994771a10ccd4e2ee22fb7b2df96a25b8010
v00rix <vladogim97@gmail.com>
2018-07-19 12:43:36 +0200

    Error handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,17 @@
 // OpenGL
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+// other stuff
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <vector>
 #include "util/Vector.h"
 
-static GLFWwindow *window;
-
+#define _DEBUG 1
 
 //region Helpers
+static GLFWwindow *window;
 
 /**
  * Initialize GLFW
@@ -71,7 +72,7 @@ static bool initGLEW() {
  * @param fs Path to fragment shader file
  * @return Program id
  */
-static unsigned int loadShaders(const char* vs, const char* fs) {
+static unsigned int loadShaders(const char *vs, const char *fs) {
 
     // Create the shaders
     GLuint VertexShaderID = glCreateShader(GL_VERTEX_SHADER);
@@ -80,12 +81,12 @@ static unsigned int loadShaders(const char* vs, const char* fs) {
     // Read the Vertex Shader code from the file
     std::string VertexShaderCode;
     std::ifstream VertexShaderStream(vs, std::ios::in);
-    if(VertexShaderStream.is_open()){
+    if (VertexShaderStream.is_open()) {
         std::stringstream sstr;
         sstr << VertexShaderStream.rdbuf();
         VertexShaderCode = sstr.str();
         VertexShaderStream.close();
-    }else{
+    } else {
         printf("Couldn't open shader file: %s.\n", vs);
         getchar();
         return 0;
@@ -94,7 +95,7 @@ static unsigned int loadShaders(const char* vs, const char* fs) {
     // Read the Fragment Shader code from the file
     std::string FragmentShaderCode;
     std::ifstream FragmentShaderStream(fs, std::ios::in);
-    if(FragmentShaderStream.is_open()){
+    if (FragmentShaderStream.is_open()) {
         std::stringstream sstr;
         sstr << FragmentShaderStream.rdbuf();
         FragmentShaderCode = sstr.str();
@@ -106,30 +107,30 @@ static unsigned int loadShaders(const char* vs, const char* fs) {
 
     // Compile Vertex Shader
     printf("Compiling shader : %s\n", vs);
-    char const * VertexSourcePointer = VertexShaderCode.c_str();
-    glShaderSource(VertexShaderID, 1, &VertexSourcePointer , nullptr);
+    char const *VertexSourcePointer = VertexShaderCode.c_str();
+    glShaderSource(VertexShaderID, 1, &VertexSourcePointer, nullptr);
     glCompileShader(VertexShaderID);
 
     // Check Vertex Shader
     glGetShaderiv(VertexShaderID, GL_COMPILE_STATUS, &Result);
     glGetShaderiv(VertexShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
-    if ( InfoLogLength > 0 ){
-        std::vector<char> VertexShaderErrorMessage((unsigned)InfoLogLength+1);
+    if (InfoLogLength > 0) {
+        std::vector<char> VertexShaderErrorMessage((unsigned) InfoLogLength + 1);
         glGetShaderInfoLog(VertexShaderID, InfoLogLength, nullptr, &VertexShaderErrorMessage[0]);
         printf("%s\n", &VertexShaderErrorMessage[0]);
     }
 
     // Compile Fragment Shader
     printf("Compiling shader : %s\n", fs);
-    char const * FragmentSourcePointer = FragmentShaderCode.c_str();
-    glShaderSource(FragmentShaderID, 1, &FragmentSourcePointer , nullptr);
+    char const *FragmentSourcePointer = FragmentShaderCode.c_str();
+    glShaderSource(FragmentShaderID, 1, &FragmentSourcePointer, nullptr);
     glCompileShader(FragmentShaderID);
 
     // Check Fragment Shader
     glGetShaderiv(FragmentShaderID, GL_COMPILE_STATUS, &Result);
     glGetShaderiv(FragmentShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
-    if ( InfoLogLength > 0 ){
-        std::vector<char> FragmentShaderErrorMessage((unsigned)InfoLogLength+1);
+    if (InfoLogLength > 0) {
+        std::vector<char> FragmentShaderErrorMessage((unsigned) InfoLogLength + 1);
         glGetShaderInfoLog(FragmentShaderID, InfoLogLength, nullptr, &FragmentShaderErrorMessage[0]);
         printf("%s\n", &FragmentShaderErrorMessage[0]);
     }
@@ -144,8 +145,8 @@ static unsigned int loadShaders(const char* vs, const char* fs) {
     // Check the program
     glGetProgramiv(ProgramID, GL_LINK_STATUS, &Result);
     glGetProgramiv(ProgramID, GL_INFO_LOG_LENGTH, &InfoLogLength);
-    if ( InfoLogLength > 0 ){
-        std::vector<char> ProgramErrorMessage((unsigned)InfoLogLength+1);
+    if (InfoLogLength > 0) {
+        std::vector<char> ProgramErrorMessage((unsigned) InfoLogLength + 1);
         glGetProgramInfoLog(ProgramID, InfoLogLength, nullptr, &ProgramErrorMessage[0]);
         printf("%s\n", &ProgramErrorMessage[0]);
     }
@@ -157,6 +158,37 @@ static unsigned int loadShaders(const char* vs, const char* fs) {
     glDeleteShader(FragmentShaderID);
 
     return ProgramID;
+}
+
+/**
+ * Error handler function
+ * @param source
+ * @param type
+ * @param id
+ * @param severity
+ * @param length
+ * @param message
+ * @param userParam
+ */
+static void APIENTRY openglCallbackFunction(
+        GLenum source,
+        GLenum type,
+        GLuint id,
+        GLenum severity,
+        GLsizei length,
+        const GLchar *message,
+        const void *userParam) {
+    (void) source;
+    (void) type;
+    (void) id;
+    (void) severity;
+    (void) length;
+    (void) userParam;
+    fprintf(stderr, "%s\n", message);
+    if (severity == GL_DEBUG_SEVERITY_HIGH) {
+        fprintf(stderr, "Aborting...\n");
+        abort();
+    }
 }
 //endregion
 
@@ -170,12 +202,26 @@ int main() {
     glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
     //endregion
 
+#if _DEBUG
+    // Request a debug context.
+    std::cout << "Debug output enabled!" << std::endl;
+    glEnable(GL_DEBUG_OUTPUT);
+    glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+    glDebugMessageCallback(openglCallbackFunction, nullptr);
+#endif
+
     std::cout << glGetString(GL_VERSION) << std::endl;
 
     const float vertices[] = {
-            -1.0f, -1.0f, 0.0f,
-            1.0f, -1.0f, 0.0f,
-            0.0f, 1.0f, 0.0f
+            -1.0f, -1.0f, 0.0f,     // 0
+            1.0f, -1.0f, 0.0f,      // 1
+            1.0f, 1.0f, 0.0f,       // 2
+            -1.0f, 1.0f, 0.0f       // 3
+    };
+
+    unsigned int indices[]{
+            0, 1, 2,
+            2, 3, 0
     };
 
     GLuint vertexArrayID;
@@ -187,17 +233,25 @@ int main() {
     glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glUseProgram(loadShaders("shaders/vertex.glsl", "shaders/fragment.glsl")); // this is relative to target binary location
+    glUseProgram(
+            loadShaders("shaders/vertex.glsl", "shaders/fragment.glsl")); // this is relative to target binary location
+
+    GLuint indexBuffer;
+    glGenBuffers(1, &indexBuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 
     do {
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         //region Main loop
-
         glEnableVertexAttribArray(0);
         glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+
         glDisableVertexArrayAttrib(vertexBuffer, 0);
         //endregion
 
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -3,6 +3,5 @@
 layout(location = 0) in vec3 position;
 
 void main() {
-    gl_Position.xyz = position;
-    gl_Position.w = 1.0;
+    gl_Position = vec4(.5 * position.x, .5 * position.y, .5*position.z, 1.0);
 }
